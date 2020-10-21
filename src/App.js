1145c73ee2e9da41d5abb1b360607746bd546a4b
Harry Gates <harry.gates@holidayextras.com>
2020-10-21 13:38:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Payment from './containers/Payment'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="navbar navbar-default navbar-static-top pt-4 mb-5">
+        <div className='container'>
+          <h1 className="text-secondary">Oliday Xtraz</h1>
+        </div>
       </header>
+      <div className='container'>
+        <Payment />
+      </div>
     </div>
   );
 }
---
+++ src/containers/Payment.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import Basket from '../components/Basket'
+
+// const Payment = () => {
+//   const [paid, setPaid] = useState(false)
+//   const product = {
+//     name: 'Room at Trump Tower',
+//     price: '$10',
+//     description: 'Bestest product, 5 mins from a road somewhere and a local tree'
+//   }
+
+//   const makePayment = () => {
+//     setPaid(true)
+//   }
+
+//   return (
+//     <div class="container">
+//       <div className='col-xs-8 text-center'>
+//         <h1>Payment Page</h1>
+//         <h2>You're booking this insane product!</h2>
+//           <button className='btn btn-xl btn-primary' onClick={makePayment}>Pay Now!</button>
+//           <p>paid: {paid} total: {product.price}</p>
+//       </div>
+
+//       <div className='col-xs-4'>
+//         <Basket product={product}/>
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default Payment
+
+const Payment = () => {
+  const product = {
+    name: 'Room at Trump Tower',
+    price: 10,
+    description: 'Bestest product, 5 mins from a road somewhere and a local tree'
+  }
+
+  const upgrades = [{
+    name: 'Dinner for 900 people',
+    price: 1,
+  },
+  {
+    name: 'Lessons how to backflip',
+    price: 2,
+  }]
+
+  const [paid, setPaid] = useState(false)
+  const [total, setTotal] = useState(product.price)
+
+  const updateTotal = (itemPrice) => {
+    setTotal(total + itemPrice)
+  }
+
+  const makePayment = () => {
+    setPaid(true)
+  }
+
+  return (
+    <div class="container">
+      <div className='col-xs-8 text-center'>
+        <h1>Payment Page</h1>
+        <h2>You're booking this insane product!</h2>
+        {paid ? (
+          <h4 className='text-success'>Successful Payment! Amount: ${total}</h4>
+        ) : (
+          <button className='btn btn-xl btn-primary' onClick={makePayment}>Pay Now!</button>
+        )}
+      </div>
+
+      <div className='col-xs-4'>
+        {!paid && <Basket
+          product={product}
+          upgrades={upgrades}
+          total={total}
+          updateTotal={updateTotal}
+        />}
+      </div>
+
+    </div>
+  )
+}
+
+export default Payment
